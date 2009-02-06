346a60ff5fe7383edcd0e4a8f4a0fe5073e5f9d9
Tom Rix <Tom.Rix@windriver.com>
2009-02-05 18:26:53 -0600

    Change CPPFLAGS from building with 2008q3 tool chain.
    Tool chain can be downloaded from:
    http://www.codesourcery.com/gnu_toolchains/arm/releases/2008q3
    
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>
    Signed-off-by: Tom Rix <Tom.Rix@windriver.com>

--- cpu/omap3/config.mk
+++ cpu/omap3/config.mk
@@ -23,7 +23,7 @@
 PLATFORM_RELFLAGS += -fno-strict-aliasing  -fno-common -ffixed-r8 \
 	-msoft-float
 
-PLATFORM_CPPFLAGS += -march=armv7a
+PLATFORM_CPPFLAGS += -march=armv7-a
 # =========================================================================
 #
 # Supply options according to compiler version
