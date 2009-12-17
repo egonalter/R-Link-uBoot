4a6cde56998d302e2929c2819b5ac139753fba51
Sukumar Ghorai <s-ghorai@ti.com>
2009-12-17 15:31:37 -0600

    Update MMC command to support MMC2 and read/write in OMAP3
    Boot CMD added for mmc init, mmc read and mmc write.
    Examples:
    mmcinit 0 or mmc init 0
    
    fatload mmc 0 80000000 mlo
    mmc write 1 80000000 0 2A
    
    fatload mmc 0 80000000 u-boot.bin
    mmc write 1 80000000 400 16B
    
    read.i and write.i command also supported.
    
    Signed-off-by: Sukumar Ghorai<s-ghorai@ti.com>
    Signed-off-by: Christina Warren <cawarren@ti.com>

--- common/cmd_mmc.c
+++ common/cmd_mmc.c
@@ -28,18 +28,139 @@
 
 #include <mmc.h>
 
+static inline int str2long(char *p, unsigned int *num)
+{
+	char *endptr;
+
+	*num = simple_strtoul(p, &endptr, 16);
+	return (*p != '\0' && *endptr == '\0') ? 1 : 0;
+}
+
 int do_mmc (cmd_tbl_t *cmdtp, int flag, int argc, char *argv[])
 {
-	if (mmc_init (1) != 0) {
-		printf ("No MMC card found\n");
-		return 1;
+	int  ret = -1;
+	unsigned int dev_num, addr = 0, offset = 0, size = 0, total;
+
+	switch (argc) {
+	case 2:
+	if ((strncmp(argv[0], "mmcinit", 7) == 0) &&
+					(argv[0][7] == '\0')) {
+		if (!(str2long(argv[1], &dev_num))) {
+			printf("'%s' is not a number\n", argv[1]);
+			goto usage;
+			}
+		if ((dev_num != 0) && (dev_num != 1))
+			goto usage;
+		ret = mmc_init(dev_num);
+		if (ret)
+			printf("No MMC card found!\n");
+		else
+			printf("MMC%d Initalization OK\n", dev_num+1);
+
+		return ret;
+		}
+		break;
+	case 3:
+	if ((strncmp(argv[0], "mmc", 3) == 0) && (argv[0][3] == '\0')) {
+
+		if ((strncmp(argv[1], "init", 4) == 0)
+					&& (argv[1][4] == '\0')) {
+			if (!(str2long(argv[2], &dev_num))) {
+				printf("'%s' is not a number\n", argv[2]);
+				goto usage;
+		}
+			if ((dev_num != 0) && (dev_num != 1))
+				goto usage;
+			ret = mmc_init(dev_num);
+			if (ret)
+				printf("No MMC card found!\n");
+			else
+				printf("MMC%d Initalization OK\n", dev_num+1);
+
+			return ret;
+		} else if (argc  == 6) {
+			total = 0;
+			/* validate the input */
+			if (!(str2long(argv[2], &dev_num))) {
+				printf("'%s' is not a number\n", argv[2]);
+				goto usage;
+			} else if (!(str2long(argv[3], &addr))) {
+				printf("'%s' is not a number\n", argv[3]);
+				goto usage;
+			} else if (!(str2long(argv[4], &offset))) {
+				printf("'%s' is not a number\n", argv[4]);
+				goto usage;
+			} else if (!(str2long(argv[5], &size))) {
+				printf("'%s' is not a number\n", argv[5]);
+				goto usage;
+			}
+			/* validate the params */
+			if (((dev_num != 0) && (dev_num != 1)) || (size == 0))
+				ret = -1;
+			else
+				ret = 0;
+		}
+
+			if (ret)
+				goto usage;
+
+		if ((strncmp(argv[1], "write.i", 7) == 0)
+						&& (argv[1][7] == '\0')) {
+			ret = mmc_write_opts(dev_num,
+				offset, size, (unsigned char *)addr, &total);
+			printf("%d bytes written: %s\n",
+				total, ret ? "OK" : "ERROR");
+		} else if ((strncmp(argv[1], "read.i", 6) == 0) &&
+						(argv[1][6] == '\0')) {
+			ret = mmc_read_opts(dev_num, offset, size,
+				(unsigned char *)addr);
+			printf("%d bytes read: %s\n",
+				size, ret ? "OK" : "ERROR");
+		} else if ((strncmp(argv[1], "write", 5) == 0) &&
+				(argv[1][5] == '\0')) {
+			ret = mmc_write_block(dev_num, offset, size,
+						(unsigned char *)addr, &total);
+			printf("%d sectors written: %s\n",
+				total, ret ? "OK" : "ERROR");
+		} else if ((strncmp(argv[1], "read", 4) == 0) &&
+							(argv[1][4] == '\0')) {
+			ret = mmc_read_block(dev_num, offset, size,
+						(unsigned char *)addr);
+			printf("Sectors read: %s\n", ret ? "OK" : "ERROR");
+			} else {
+
+			goto usage;
+			}
+		/* status of the operation */
+		return ret;
+		}
+
+		break;
+
+	default:
+		goto usage;
+		break;
 	}
-	return 0;
+
+usage:
+	printf("Usage:\n%s\n", cmdtp->usage);
+	return 1;
 }
 
 U_BOOT_CMD(
-	mmcinit,	1,	0,	do_mmc,
-	"mmcinit - init mmc card\n",
+	mmc, 6, 1, do_mmc,
+	"mmc - MMC sub-system\n"
+	"mmc init <dev>\n"
+	"mmc read[.i]/write[.i] <dev> <addr> <block offset> [size]\n",
+	"mmc init - device (0 for MMC1, 1 for MMC2)\n"
+	"mmc read[.i] device addr offset size - \n"
+	"mmc write[.i] device addr offset size - \n"
+	"--- write `size' starting at offset `offset' to/from eMMC device\n"
+	"device - 0/1 for MMC1/MMC2\n" "`addr' is memory location\n"
+);
+U_BOOT_CMD(
+	mmcinit, 2, 0, do_mmc,
+	"mmcinit <dev> - init mmc card (0 for MMC1, 1 for MMC2)\n",
 	NULL
 );
 
