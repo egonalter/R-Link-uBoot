248783f15092779da506801e97936aa2b7ed3783
Sukumar Ghorai <s-ghorai@ti.com>
2010-02-18 11:35:38 +0530

    Add support for valid cluster number for FAT32
    This patch fixes problems seen when trying to fatload large files
    i.e. files >100MB.
    
    "Invalid FAT entry" message observed on FAT32 when file size
    crosses 0xfff0 cluster. If FAT32, then the valid cluster is 0x0ffffff0
    and not 0xfff0.
    
    Signed-off-by: Sukumar Ghorai <s-ghorai@ti.com>

--- fs/fat/fat.c
+++ fs/fat/fat.c
@@ -345,7 +345,7 @@ get_contents(fsdata *mydata, dir_entry *dentptr, __u8 *buffer,
 			newclust = get_fatent(mydata, endclust);
 			if((newclust -1)!=endclust)
 				goto getit;
-			if (newclust <= 0x0001 || newclust >= 0xfff0) {
+			if (CHECK_CLUST(newclust, mydata->fatsize)) {
 				FAT_DPRINT("curclust: 0x%x\n", newclust);
 				FAT_DPRINT("Invalid FAT entry\n");
 				return gotsize;
@@ -380,7 +380,7 @@ getit:
 		filesize -= actsize;
 		buffer += actsize;
 		curclust = get_fatent(mydata, endclust);
-		if (curclust <= 0x0001 || curclust >= 0xfff0) {
+		if (CHECK_CLUST(curclust, mydata->fatsize)) {
 			FAT_DPRINT("curclust: 0x%x\n", curclust);
 			FAT_ERROR("Invalid FAT entry\n");
 			return gotsize;
@@ -452,7 +452,7 @@ get_vfatname(fsdata *mydata, int curclust, __u8 *cluster,
 
 		slotptr--;
 		curclust = get_fatent(mydata, curclust);
-		if (curclust <= 0x0001 || curclust >= 0xfff0) {
+		if (CHECK_CLUST(curclust, mydata->fatsize)) {
 			FAT_DPRINT("curclust: 0x%x\n", curclust);
 			FAT_ERROR("Invalid FAT entry\n");
 			return -1;
@@ -645,10 +645,10 @@ static dir_entry *get_dentfromdir (fsdata * mydata, int startsect,
 	    return retdent;
 	}
 	curclust = get_fatent (mydata, curclust);
-	if (curclust <= 0x0001 || curclust >= 0xfff0) {
-	    FAT_DPRINT ("curclust: 0x%x\n", curclust);
-	    FAT_ERROR ("Invalid FAT entry\n");
-	    return NULL;
+	if (CHECK_CLUST(curclust, mydata->fatsize)) {
+		FAT_DPRINT("curclust: 0x%x\n", curclust);
+		FAT_ERROR("Invalid FAT entry\n");
+		return NULL;
 	}
     }
 
